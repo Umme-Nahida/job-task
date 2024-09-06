6ed52150293a9a88e8b4d2db7bdeb16b5bcddcfe
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-06 11:24:13 +0600

    added some image in job task project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import router from './router/Router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
