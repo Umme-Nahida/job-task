b97d48165bf56257d3334891f3d9a0f5abd7a123
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-06 21:44:46 +0600

    install firebase

---
+++ src/Authentication/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import auth from '../firebase.config';
+
+const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+    const[user,setUser] = useState(null)
+    const [loading,setLoading] = useState(true)
+
+
+    const createUser=(email,password)=>{
+       setLoading(true)
+       return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const loginUser=(email,password)=>{
+       setLoading(true)
+       return signInWithEmailAndPassword(auth,email,password)
+    }
+
+  useEffect(()=>{
+   const unSubscribe = onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser)
+        setLoading(false)
+        console.log(currentUser)
+    })
+
+    return ()=>{
+        return unSubscribe()
+    }
+  },[])
+
+    const authInfo ={
+        user,
+        createUser,
+        loginUser
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
