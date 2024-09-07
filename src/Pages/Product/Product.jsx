8205ef8b3a38b710efb499430f4c7e5a0f61fe6a
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-07 09:30:57 +0600

    create hooks for load product data

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { productContext } from '../../ProductProvider/ProductProvider';
 
 const Product = () => {
+    const {product}=useContext(productContext);
+    
     return (
         <div>
-            product data here
+            
+             {/* product category */}
+            <div className='min-h-screen bg-yellow-200'>
+                <ul className='menu space-y-3'>
+                    <li></li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/ProductProvider/ProductProvider.jsx
+++ src/ProductProvider/ProductProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const productContext = createContext()
 
@@ -6,10 +6,29 @@ const ProductProvider = ({children}) => {
     const [product,setProduct]=useState([])
     const [cart,setCart]=useState([])
 
+    useEffect(()=>{
+        fetch('/product.json')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setProduct(data)
+        })
+    },[])
 
+    // add to cart functionality
+    const addToCart =(product)=>{
+       setCart((preCart)=> [...preCart,product])
+    }
+
+    // delete item functionality
+    const deleteFromCart =(productId)=>{
+        setCart((preCart)=> preCart.filter(item=>item.id !== productId))
+    }
     const productInfo= {
         product,setProduct,
-        cart,setCart
+        cart,setCart,
+        addToCart,
+        deleteFromCart
     }
 
 
